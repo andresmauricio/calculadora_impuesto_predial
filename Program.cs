f3d9a9c44f3f878af6b5093609e98348180e6591
andresmauricio <andresacelas@outlook.com>
2020-04-11 22:13:19 -0500

    init project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace learning_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WritingHello("Hola bebes!");
+        }
+
+        static void WritingHello(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
